ed0d7afa111fc5abd763bc20f386bef0b5a2ee2d
phyroxin <campslamin@blueyonder.co.uk>
2014-08-12 18:10:58 +0100

    12/08/2014

--- public/build/pixi-action.js
+++ public/build/pixi-action.js
@@ -1,27 +1,32 @@
 (function($, pixi, win){
 	
+	/*======================================================================
+	 * Config
+	 *======================================================================
+	 */
 	//create stage and add colour
-	var  interactive 			= true
-		,imgDIR 				= '../../../images/'
-		,stage 					= new pixi.Stage(0xcccccc, interactive)
-		,bunnyTexture			= pixi.Texture.fromImage(imgDIR+"bunny.png")
-		,redTexture				= pixi.Texture.fromImage(imgDIR+"astroboy.fw-small.fw.png")
-		,backgroundTexture  	= pixi.Texture.fromImage(imgDIR+"pixel_landscape.jpg")
-		,canvas					= '#pixi-wrap > canvas'
+	var  interactive 			 = true
+		,imgDIR 				 = '../../../images/'
+		,stage 					 = new pixi.Stage(0xcccccc, interactive)
+		,bunnyTexture			 = pixi.Texture.fromImage(imgDIR+"bunny.png")
+		,redTexture				 = pixi.Texture.fromImage(imgDIR+"astroboy.fw-small.fw.png")
+		,backgroundTexture  	 = pixi.Texture.fromImage(imgDIR+"pixel_landscape.jpg")
+		,canvas					 = '#pixi-wrap > canvas'
+		,direction 				 = 'right'
 		,count = widthAdjust = n = 0
-		,direction 				= 'right'
-		,padding				= 20
-		,heightAdjust			= 300
-		,bunnyArray   			= []
-		,redArray   			= []
-		,bunnyCount   			= 2
-		,bunnyMax	  			= 500
-		,globalDelay	  		= 10
-		,globalDuration	  		= 1000;
+		,bunnyCount   			 = 1
+		,globalDelay	  		 = 20
+		,padding				 = 20
+		,bunnyMax	  			 = 500
+		,heightAdjust			 = 300
+		,globalDuration	  		 = 1000
+		,redArray   			 = []
+		,bunnyArray   			 = [];
 		
 	var config = {
 		 windowWidth  : (win.innerWidth || win.documentElement.clientWidth || win.body.clientWidth) - widthAdjust
 		,windowHeight : (win.innerHeight || win.documentElement.clientHeight || win.body.clientHeight) - heightAdjust
+		,canJump : true
 	};
 		
 	// You can use either WebGLRenderer or CanvasRenderer
@@ -34,7 +39,7 @@
 	 * Animation core
 	 *======================================================================
 	 */
-	function animate(opts, cb){
+	function animate(opts, cb){ 
 		var start = new Date;
 		var id = setInterval(function(){
 			var timePassed  = new Date - start;
@@ -52,172 +57,13 @@
 					cb();
 			}
 				
-		}, opts.delay || 20);
-	}
-
-	function checkPos(){
-		if(bunny.position.y >= (config['windowHeight'] - (bunny.height/2)))
-			direction = 'left';
-		else if(bunny.position.x <= 0 + (bunny.width/2))
-			direction = 'right';
-			
-		count = count +1;
-	}
-
-	function frameCount() {
-		var text = count + ' fps'
-		document.getElementById("pixi-text-wrap").innerHTML = text;
-		count = 0;
-	};
-
-	function init(duration){
-		
-		if(n >= bunnyMax)
-			return;
-		
-		for(i=0; i<bunnyCount; i=i+1, n=n+1){
-		
-			var  randChoice = randomScale(1, 100)
-				,redsHere   = false;
-			
-			if(randChoice %27 === 0){
-				bunnyArray[n] = new pixi.Sprite(redTexture);
-				redsHere = true;
-			}
-			else{
-				bunnyArray[n] = new pixi.Sprite(bunnyTexture);
-				redsHere = false;
-			}
-				
-			bunnyArray[n].position.x    = randomScale(10, config['windowWidth']);
-			bunnyArray[n].position.y    = -20;
-			bunnyArray[n].width			= (redsHere)?20:27;
-			bunnyArray[n].height 	    = (redsHere)?25:53;
-			bunnyArray[n].pivot.x 	    = (bunnyArray[n].width /2);
-			bunnyArray[n].pivot.y 	    = (bunnyArray[n].height /((redsHere)?2:randomScale(2,4)));
-			bunnyArray[n].scale.x 	    = 1;
-			bunnyArray[n].scale.y 	    = 1;
-			bunnyArray[n].buttonMode    = true;
-			bunnyArray[n].alpha		    = 1;
-			bunnyArray[n].defaultCursor = 'pointer';
-			
-			bunnyArray[n].setInteractive(true);
-			
-			bunnyArray[n].click = function(){
-				console.log('clicked bunny');
-			};
-			
-			bunnyArray[n].mouseover = function(){
-				bunnyJump(this, 300);
-			};
-
-			stage.addChild(bunnyArray[n]);
-				
-			if(redsHere){
-				redArray.push(bunnyArray[n]);
-				bunnyArray.slice(n);
-			}
-			
-			(function(ele){
-				var to = config['windowHeight'] - (ele.pivot.y +50);
-				animate({
-					 delay: globalDelay
-					,duration: globalDuration
-					,direction: direction
-					,delta: bounceEaseOut
-					,elem: ele
-					,step: function(delta, elem){
-						elem.position.y = to*delta;
-						renderer.render(stage);
-					}
-				});
-			}(bunnyArray[n]));
-		}
-		
-		var  arrayBackRow	  = []
-			,arrayMidBackRow  = []
-			,arrayMidFrontRow = []
-			,arrayFrontRow	  = []
-			,newBunnyArr	  = [];
-		
-		$.each(bunnyArray, function(){
-			
-			stage.removeChild(this);
-			if(this.pivot.y === 26.5)
-				arrayBackRow.push(this);
-			else if(this.pivot.y === 17.666666666666668)
-				arrayMidBackRow.push(this);
-			else if(this.pivot.y === 13.25)
-				arrayMidFrontRow.push(this);
-			else if(this.pivot.y === 10.6)
-				arrayFrontRow.push(this);
-		});
-		
-		newBunnyArr.push(arrayBackRow);
-		newBunnyArr.push(arrayMidBackRow);
-		newBunnyArr.push(arrayMidFrontRow);
-		newBunnyArr.push(arrayFrontRow);
-			
-		$.each(newBunnyArr, function(){
-			$.each(this, function(){
-				stage.addChild(this);
-			});
-		});
-		
-		$.each(redArray, function(key, item){	
-			stage.addChildAt(this, stage.children.length - 1);
-		});
-	};
-	
-	function drawBackground(){
-		backTexture = new pixi.Sprite(backgroundTexture);
-		backTexture.position.x  = 0;
-		backTexture.position.y  = 0;
-		backTexture.scale.y		= 0.5;
-		backTexture.scale.y		= 0.5;
-		
-		stage.addChild(backTexture);
-		renderer.render(stage);
+		}, globalDelay);
 	}
 	
-	function bunnyDrop(element, duration){
-		
-		if(typeof element === 'undefined')
-			return;
-			
-		var to = config['windowHeight'] - (element.pivot.y +50);
-		
-		animate({
-			 delay: globalDelay
-			,duration: duration || globalDuration
-			,delta: bounceEaseOut
-			,elem: element
-			,step: function(delta, elem){
-				elem.position.y = to*delta;
-				renderer.render(stage);
-			}
-		});
-	}
-	
-	function bunnyJump(element, duration){
-		
-		if(typeof element === 'undefined')
-			return;
-			
-		var to = 100;
-			
-		animate({
-			 delay: globalDelay
-			,duration: duration || globalDuration
-			,delta: quadEaseOut
-			,elem: element
-			,step: function(delta, elem){
-				elem.position.y -= (to*delta);
-				renderer.render(stage);
-			}
-		}, function(){ bunnyDrop(element, 500) });
-	}
-	
+	/*======================================================================
+	 * Tween algo's
+	 *======================================================================
+	 */
 	function randomScale(start, end){
 		return Math.floor(Math.random() * end) + start;
 	}
@@ -256,39 +102,266 @@
 		}
 	};
 	
-	var quadEaseOut = makeEaseOut(quad);
+	var quadEaseOut		= makeEaseOut(quad);
+	var bounceEaseOut	= makeEaseOut(bounce);
+	var elasticEaseOut	= makeEaseOut(elastic);
 	
-	var bounceEaseOut = makeEaseOut(bounce);
 	
-	var elasticEaseOut = makeEaseOut(elastic);
-
-	$(win).resize(function(){
-
-		config['windowWidth']  = (win.innerWidth || win.documentElement.clientWidth || win.body.clientWidth) - widthAdjust;
-		config['windowHeight'] = (win.innerHeight || win.documentElement.clientHeight || win.body.clientHeight) - heightAdjust;
+	/*======================================================================
+	 * Draw sprites
+	 *======================================================================
+	 */
+	function init(duration){
 		
-		$(canvas)
-			.attr('width',  config['windowWidth'])
-			.attr('height', config['windowHeight']);
-	});
+		if(n >= bunnyMax)
+			return;
+		
+		for(i=0; i<bunnyCount; i=i+1, n=n+1){
+		
+			var  randChoice = randomScale(1, 100)
+				,redsHere   = false;
+			
+			if(randChoice %27 === 0){
+				bunnyArray[n] = new pixi.Sprite(redTexture);
+				redsHere = true;
+			}
+			else{
+				bunnyArray[n] = new pixi.Sprite(bunnyTexture);
+				redsHere = false;
+			}
+				
+			bunnyArray[n].position.x    = randomScale(10, config['windowWidth']);
+			bunnyArray[n].position.y    = -20;
+			bunnyArray[n].width			= (redsHere)?20:27;
+			bunnyArray[n].height 	    = (redsHere)?25:53;
+			bunnyArray[n].pivot.x 	    = (bunnyArray[n].width /2);
+			bunnyArray[n].pivot.y 	    = (bunnyArray[n].height /((redsHere)?2:randomScale(2,4)));
+			bunnyArray[n].scale.x 	    = 1;
+			bunnyArray[n].scale.y 	    = 1;
+			bunnyArray[n].buttonMode    = true;
+			bunnyArray[n].alpha		    = 1;
+			bunnyArray[n].defaultCursor = 'pointer';
+			bunnyArray[n].setInteractive(true);
+			bunnyArray[n].canJump		= true;
+			bunnyArray[n].canMove		= true;
+
+			bunnyArray[n].mouseover = function(){
+				bunnyJump(this, 300);
+			};
+
+			if(redsHere){
+				redArray.push(bunnyArray[n]);
+				bunnyArray.slice(n);
+			}
+			
+			stage.addChild(bunnyArray[n]);
+
+			(function(ele){
+				var to = config['windowHeight'] - (ele.pivot.y +50);
+				animate({
+					 delay		: globalDelay
+					,duration	: globalDuration
+					,direction	: direction
+					,delta		: bounceEaseOut
+					,elem		: ele
+					,step: function(delta, elem){
+						elem.position.y = to*delta;
+					}
+				});
+			}(bunnyArray[n]));
+		}
+		
+		sortSprites();
+	};
 	
-	drawBackground();
-	
-	$(document)
-		.on('click', canvas, function(){
-			console.log('canvas clicked');
-			bunnyJump(bunnyArray[randomScale(1, n)], 2000);
+	function sortSprites(){
+			
+		var  arrayBackRow	  = []
+			,arrayMidBackRow  = []
+			,arrayMidFrontRow = []
+			,arrayFrontRow	  = []
+			,newBunnyArr	  = [];
+		
+		$.each(bunnyArray, function(){
+			
+			stage.removeChild(this);
+			if(this.pivot.y === 26.5)
+				arrayBackRow.push(this);
+			else if(this.pivot.y === 17.666666666666668)
+				arrayMidBackRow.push(this);
+			else if(this.pivot.y === 13.25)
+				arrayMidFrontRow.push(this);
+			else if(this.pivot.y === 10.6)
+				arrayFrontRow.push(this);
 		});
+		
+		newBunnyArr.push(arrayBackRow);
+		newBunnyArr.push(arrayMidBackRow);
+		newBunnyArr.push(arrayMidFrontRow);
+		newBunnyArr.push(arrayFrontRow);
+			
+		$.each(newBunnyArr, function(){
+			$.each(this, function(){
+				stage.addChild(this);
+			});
+		});
+		
+		$.each(redArray, function(key, item){	
+			stage.addChildAt(this, stage.children.length - 1);
+		});
+	}
 	
-	(function(){
+	function drawBackground(){
+		backTexture = new pixi.Sprite(backgroundTexture);
+		backTexture.position.x  = 0;
+		backTexture.position.y  = 0;
+		backTexture.scale.y		= 0.5;
+		backTexture.scale.y		= 0.5;
+		
+		stage.addChild(backTexture);
+		renderer.render(stage);
+	}
+	
+	
+	/*======================================================================
+	 * Sprite actions
+	 *======================================================================
+	 */
+	function bunnyDrop(element, duration){
+		
+		if(typeof element === 'undefined')
+			return;
+		
+		var to	 = config['windowHeight'] - ((element.pivot.y +50)+element.position.y);
+		var from = element.position.y;
+		
+		animate({
+			 delay: globalDelay
+			,duration: duration || globalDuration
+			,delta: bounceEaseOut
+			,elem: element
+			,step: function(delta, elem){
+				elem.position.y = from+(to*delta);
+			}
+		}, function(){ element.canJump = true; });
+	}
+	
+	function bunnyJump(element, duration){
+		
+		if(typeof element === 'undefined')
+			return;
+			
+		element.canJump = false;
+		var to	 = randomScale(100, 300);
+		var from = element.position.y;
+			
+		animate({
+			 delay: globalDelay
+			,duration: duration || globalDuration
+			,delta: quadEaseOut
+			,elem: element
+			,step: function(delta, elem){
+				elem.position.y = from-(to*delta);
+			}
+		}, function(){ bunnyDrop(element, 500) });
+	}
+	
+	function bunnyMove(element, duration){
+		
+		if(typeof element === 'undefined')
+			return;
+		
+		element.canMove = false;
+		var to	 = randomScale(50, 500);
+		var from = element.position.x;
+			
+		animate({
+			 delay: globalDelay
+			,duration: duration || globalDuration
+			,delta: quadEaseOut
+			,elem: element
+			,step: function(delta, elem){
+
+				if(from > to)
+					elem.position.x = from-(to*delta);
+				else if(from < to)
+					elem.position.x = from+(to*delta);
+			}
+		}, function(){ element.canMove = true; });
+	}
+	
+	/*======================================================================
+	 * Spawn action
+	 *======================================================================
+	 */
+	// random move
+	(function(move){
+		var randomMove = setInterval(function(){
+			var randInt	  = randomScale(1, 100);
+			var randBunny = randomScale(1, n);
+			//if(randInt %2 === 0)
+				if(typeof bunnyArray[randBunny] !== 'undefined' && bunnyArray[randBunny].canMove === true)
+					move(bunnyArray[randBunny], 500);
+			
+		}, 300);
+	}(bunnyMove));
+	
+	// random jump
+	(function(jump){
+		var randomJump = setInterval(function(){
+			var randInt	  = randomScale(1, 100);
+			var randBunny = randomScale(1, n);
+			//if(randInt %15 === 0)
+				if(typeof bunnyArray[randBunny] !== 'undefined' && bunnyArray[randBunny].canJump === true)
+					jump(bunnyArray[randBunny], 300);
+			
+		}, 100);
+	}(bunnyJump));
+	
+	// random spawn
+	(function(drawBg){
+		drawBg();
 		var randomSpawn = setInterval(function(){
 			var randInt = randomScale(1, 100);
 			if(randInt %10 === 0)
 				init();
-			if(randInt %5 === 0)
-				bunnyJump(bunnyArray[randomScale(1, n)], 500);
-			
-		}, 1*100);
+		}, 100);
+	}(drawBackground));
+	
+	// renderer
+	(function(){
+		var renderLoop = setInterval(function(){
+			renderer.render(stage);
+		}, globalDelay);
 	}());
+	
+	
+	/*======================================================================
+	 * Event bindings
+	 *======================================================================
+	 */
+	$(win)
+		.resize(function(){
+		
+			config['windowWidth']  = (win.innerWidth || win.documentElement.clientWidth || win.body.clientWidth) - widthAdjust;
+			config['windowHeight'] = (win.innerHeight || win.documentElement.clientHeight || win.body.clientHeight) - heightAdjust;
+			
+			$(canvas)
+				.attr('width',  config['windowWidth'])
+				.attr('height', config['windowHeight']);
+		})
+		.on('click', canvas, function(){
+			var whichBunny = randomScale(1, n);
+			if(typeof bunnyArray[whichBunny] !== 'undefined' && bunnyArray[whichBunny].canJump === true)
+				bunnyJump(bunnyArray[whichBunny], 300);
+		})
+		.on('keydown',function(event) {
+			if(event.keyCode == 37) {
+				alert('Left was pressed');
+			}
+			else if(event.keyCode == 39) {
+				alert('Right was pressed');
+			}
+		});
 
 }(jQuery, PIXI || {}, window || document));
